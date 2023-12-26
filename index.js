20750da6e9505dfccdf7ea90e9fd66ea11b3d7c8
worapakorn <120018217+WorapakornJarusiriphot@users.noreply.github.com>
2023-12-26 18:52:31 +0700

    npm install cookie-parser --save And update index.js

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const User = require("./models/User");
+const Post = require("./models/Post");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const uploadMiddleware = multer({ dest: "uploads/" });
+const fs = require("fs");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json());
+app.use(cookieParser());
 
 //Databse Connection
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -58,6 +64,29 @@ app.post("/login", async (req, res) => {
 //User logout
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json("ok");
+});
+
+//Create Post
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+      author: info.id,
+    });
+    res.json(postDoc);
+  });
 });
 
 const PORT = process.env.PORT;
